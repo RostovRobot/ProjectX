e0bb3cc07a4f849a8de19a8cc2851046c4e0710f
OvsyannikovAU <ovsyannikov.a.u@gmail.com>
2016-11-10 20:11:08 +0300

    Обновление языкового пакета и первичное создание классов

---
+++ Point2D.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
+{
+    /// <summary>
+    /// Класс, описывающий точку на карте
+    /// </summary>
+    class Point2D
+    {
+        /// <summary>
+        /// Координата X точки
+        /// </summary>
+        public double X;
+        /// <summary>
+        /// Координата Y точки
+        /// </summary>
+        public double Y;
+
+        /// <summary>
+        /// Метод-конструктор с заданными координатами
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        public Point2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Метод-конструктор с заданными координатами
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        public Point2D(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        /// <summary>
+        /// Метод-конструктор без параметров (создает точку с нулевыми координатами)
+        /// </summary>
+        public Point2D()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
---
+++ Strat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk.Model;
+
+namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
+{
+    /// <summary>
+    /// Класс, описывающий стратегию игрока
+    /// </summary>
+    class Strat
+    {
+        /// <summary>
+        /// !!! НЕ РЕАЛИЗОВАНО!!! Возвращает координаты самой важной зоны
+        /// </summary>
+        /// <param name="world">Игровой мир</param>
+        /// <param name="game">Константы игры</param>
+        /// <param name="self">Собственный маг</param>
+        /// <returns></returns>
+        public Point2D getHotZone(World world, Game game, Wizard self)
+        {
+            return new Point2D();
+        }
+    }
+}
