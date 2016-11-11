0a8b500f6c7fa81fbd34792f0666439d0f30860e
Александр Николаев <roketka1@gmail.com>
2016-11-11 22:45:42 +0300

    Написал простейшую ракету. Взял алгоритм с офсайта.

--- Tactic.cs
+++ Tactic.cs
@@ -19,9 +19,48 @@ namespace Com.CodeGame.CodeWizards2016.DevKit.CSharpCgdk
         /// <param name="game">Константы игры</param>
         /// <param name="self">Собственный маг</param>
         /// <returns>Объект-движение</returns>
-        public Move getTacticMove(World world, Game game, Wizard self)
+        // Добавил move, в который мы будем присваивать дополнительное движение.
+        public Move getTacticMove(World world, Game game, Wizard self, Move move)
         {
             return new Move();
         }
+
+
+
+        
+
+
+
+
+        //
+        //Некое разделение областей работы. Ниже будут описаны действия, используемые в тактике.
+        //
+
+
+        //Простейший запуск ракеты в заданного противника. Переписан с офсайта.
+        //targetUnit здесь - цель, которую выбрали для атаки.
+        //World, Game добавил на всякий случай. Могут пригодиться.
+        public void getRocket(World world, Game game, Wizard self, LivingUnit targetUnit, Move move)
+        {
+            if(targetUnit.GetDistanceTo(targetUnit)<=self.CastRange)
+            {
+                double angle = self.GetAngleTo(targetUnit);
+                move.Turn = angle;
+                
+                if(Math.Abs(angle)<=self.VisionRange)
+                {
+                    move.Action = ActionType.MagicMissile;
+                    move.CastAngle = angle;
+                    move.MinCastDistance = self.GetDistanceTo(targetUnit) + self.Radius - targetUnit.Radius;
+                }
+
+
+            }
+
+        }
+
+
+
+
     }
 }
